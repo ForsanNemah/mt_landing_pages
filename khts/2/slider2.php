f28a58b3a83472d28e8930a9e4fddcfef0e56af5
ForsanNemah <forsan20172017@gmail.com>
2023-02-05 06:20:33 +0300

    mt 2

---
+++ khts/2/slider2.php
@@ -0,0 +1,48 @@
+<h2> كادر طبي متخصص</h2>
+<br>
+
+<div id="myCarousel2" class="carousel slide" data-ride="carousel"  >
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel2" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel2" data-slide-to="1"></li>
+      <li data-target="#myCarousel2" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" >
+
+	
+      
+
+      <div class="item active">
+        
+		<img src="ads/dr2.jpg"class="img-responsive" alt="Responsive image"   >
+		
+      </div>
+
+      <div class="item  ">
+        
+        <img src="ads/dr2.jpg"class="img-responsive" alt="Responsive image"   >
+        
+          </div>
+
+         
+
+         
+    
+       
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel2" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel2" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+
+  </div>
+  
+  </div>
---
+++ khts/2/slider3.php
@@ -0,0 +1,56 @@
+<h2> كادر طبي خبير</h2>
+<br>
+
+<div id="myCarousel3" class="carousel slide" data-ride="carousel"  >
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarouse3" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarouse3" data-slide-to="1"></li>
+      <li data-target="#myCarouse3" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" >
+
+	
+      
+
+      <div class="item active">
+        
+		<img src="ads/dr1asnan.jpg"class="img-responsive" alt="Responsive image"   >
+		
+      </div>
+
+      <div class="item  ">
+        
+        <img src="ads/dr2asnan.jpg"class="img-responsive" alt="Responsive image"   >
+        
+          </div>
+
+          <div class="item  ">
+        
+        <img src="ads/dr3asnan.jpg"class="img-responsive" alt="Responsive image"   >
+        
+          </div>
+
+          <div class="item  ">
+        
+        <img src="ads/dr4asnan.jpg"class="img-responsive" alt="Responsive image"   >
+        
+          </div>
+    
+       
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel3" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel3" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+
+  </div>
+  
+  </div>
